75c5741f7e4a13416a6354837d0c1cc376d45340
赵力 <zhaol@gzjp.cn>
2020-12-02 14:34:47 +0800

    搭建SpringCloud基础框架

---
+++ README.md
@@ -0,0 +1,11 @@
+# 项目名称：foxglove (地黄花，花语：热爱)
+# 说明：
+> 此项目用于技术研究和代码练习，欢迎大家fork共同学习进步
+# 采用技术：
+> SpringCloud、mysql、redis、nacos、OAuth2、mybatis-plus、swagger2
+# 项目结构：
+> foxglove：地黄花项目
+>> common：公共配置以及常量和工具类 <br>
+>> gateway：微服务服务网关，负责负载、路由和安全校验 <br>
+>> service：微服务模块，包含各种功能微服务 <br>
+>> service-api：api接口，提供公用的实体类和feign接口调用<br>
---
+++ gateway/src/main/java/com/zhaol/gateway/GateWayApplication.java
@@ -0,0 +1,37 @@
+/*
+ *      Copyright (c) 2018-2028, Chill Zhuang All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  Neither the name of the dreamlu.net developer nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ *  Author: Chill 庄骞 (smallchill@163.com)
+ */
+package com.zhaol.gateway;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 启动类
+ * @author 赵力
+ * @date 2020-12-02 11:28:44
+*/
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.zhaol.*.mapper")
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
